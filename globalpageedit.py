269ca1e142aef9f6d4f8e712515ff552b6c7662c
Hydriz <admin@alphacorp.tk>
2012-08-30 20:46:27 +0800

    Adding code for script.

--- globalpageedit.py
+++ globalpageedit.py
@@ -15,7 +15,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Doing nothing...
+import wikitools
 
 # Configuration
-page="" # The user page to edit
+username = ""
+password = ""
+page = "" # The user page to edit
+text = u'''
+'''
+summary = ""
+wikis = open("listofwikis.txt", 'r').read().splitlines()
+########## DANGER!!! ##### DANGER!!! ##### DANGER!!! ##########
+# IMPORTANT: WILL OVERWRITE EVERYTHING, PLEASE ADD WIKIS THAT SHOULD BE SKIPPED IN HERE!
+skipwikis = {}
+
+# Nothing to change below...
+def welcome():
+	# Welcome message, but includes some checks...
+	if (username == "") or (password == "") or (page == ""):
+		print "ERROR (1): Essential parameters undefined!"
+	else:
+		print "Welcome to the Global Page Edit tool!"
+
+def bye():
+	print "Done! Please check the amount of devastation as a result of this script..."
+
+def runthru():
+	# Function to run through the list of wikis and edit them (skipping the skipwikis list)
+	global skipwikis, wikis
+	for wiki in wikis:
+		if wiki in skipwikis:
+			print "Skipping %s.beta.wmflabs.org (in skip list)" % (wiki)
+		else:
+			print "Changing %s.beta.wmflabs.org..." % (wiki)
+			edit(wiki)
+
+def edit(wiki):
+	# Function to edit a specific page
+	global page, password, text, username
+	apiurl = "http://%s.beta.wmflabs.org/w/api.php" % (wiki)
+	wikie = wikitools.Wiki(apiurl)
+	wikie.login(username, password)
+	pagefunct = wikitools.Page(wikie, page)
+	page_text = text.encode('utf-8')
+	pagefunct.edit(page_text, summary=summary)
+
+def main():
+	welcome()
+	runthru()
+	bye()
+
+if __name__ = "__main__":
+	main()
