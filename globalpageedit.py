983ec39ab6099fda9009cd798c8d510f6328ee9e
Hydriz <admin@alphacorp.tk>
2012-09-09 12:53:14 +0800

    Adding some functions to extend the functions of the script

--- globalpageedit.py
+++ globalpageedit.py
@@ -24,13 +24,15 @@ import wikitools
 wikis = open(wikilist, 'r').read().splitlines()
 def welcome():
 	# Welcome message, but includes some checks...
-	if (username == "") or (password == "") or (page == ""):
+	if (username == "") or (password == "") or (page == "") or (action == ""):
 		print "ERROR (1): Essential parameters undefined!"
+	elif (action != "overwrite") or (action != "create") or (action != "delete") or (action != "append") or (action != "prepend"):
+		print "ERROR (2): Action is invalid!"
 	else:
 		print "Welcome to the Global Page Edit tool!"
 
 def bye():
-	print "Done! Please check the amount of devastation as a result of this script..."
+	print "Done! Please check the amount of devastation as a result of this script. :D"
 
 def runthru():
 	# Function to run through the list of wikis and edit them (skipping the skipwikis list)
@@ -39,18 +41,62 @@ def runthru():
 		if wiki in skipwikis:
 			print "Skipping %s.beta.wmflabs.org (in skip list)" % (wiki)
 		else:
-			print "Changing %s.beta.wmflabs.org..." % (wiki)
-			edit(wiki)
+			print "Modifying %s.beta.wmflabs.org..." % (wiki)
+			if (action == "overwrite"):
+				overwrite(wiki)
+			elif (action == "create"):
+				create(wiki)
+			elif (action == "delete"):
+				delete(wiki)
+			elif (action == "append"):
+				append(wiki)
+			elif (action == "prepend"):
+				prepend(wiki)
 
-def edit(wiki):
-	# Function to edit a specific page
-	global page, password, text, username
+# The overwrite function. Overwrites existing pages with new content and create non-existent pages with the set content.
+def overwrite(wiki):
+	global page, password, summary, text, username
 	apiurl = "http://%s.beta.wmflabs.org/w/api.php" % (wiki)
 	wikie = wikitools.Wiki(apiurl)
 	wikie.login(username, password)
 	pagefunct = wikitools.Page(wikie, page)
 	page_text = text.encode('utf-8')
 	pagefunct.edit(page_text, summary=summary)
+
+def create(wiki):
+	global page, password, summary, text, username
+	# Foobar
+
+def delete(wiki):
+	global page, password, summary, username
+	apiurl = "http://%s.beta.wmflabs.org/w/api.php" % (wiki)
+	wikie = wikitools.Wiki(apiurl)
+	wikie.login(username, password)
+	pagefunct = wikitools.Page(wikie, page)
+	page_text = text.encode('utf-8')
+	pagefunct.delete(reason=summary)
+
+def append(wiki):
+	global page, password, summary, text, username
+	apiurl = "http://%s.beta.wmflabs.org/w/api.php" % (wiki)
+	wikie = wikitools.Wiki(apiurl)
+	wikie.login(username, password)
+	pagefunct = wikitools.Page(wikie, page)
+	page_text = text.encode('utf-8')
+	existingtext = pagefunct.getWikiText()
+	result_text = existingtext + "\n" + page_text
+	pagefunct.edit(result_text, summary=summary)
+
+def prepend(wiki):
+	global page, password, summary, text, username
+	apiurl = "http://%s.beta.wmflabs.org/w/api.php" % (wiki)
+	wikie = wikitools.Wiki(apiurl)
+	wikie.login(username, password)
+	pagefunct = wikitools.Page(wikie, page)
+	page_text = text.encode('utf-8')
+	existingtext = pagefunct.getWikiText()
+	result_text = page_text + "\n" + existingtext
+	pagefunct.edit(result_text, summary=summary)
 
 def main():
 	welcome()
--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 # Configuration file for globalpageedit.py
 # More information is available at the repository's wiki on GitHub.
 
+# What to do? Options:
+# overwrite - Overwrite existing pages with the new set content (and create non-existent pages with the set text)
+# create - Create new pages with the set content, leaving existing pages intact. (NOT SUPPORTED)
+# delete - Delete existing pages.
+# append - Add the content set below to the bottom of existing pages (and creating non-existent pages with it)
+# prepend - Add the content set below to the top of existing pages (and create non-existent pages with it)
+action = ""
+
 # Login information
 username = ""
 password = ""
@@ -9,7 +17,7 @@ password = ""
 page = "" # The user page to edit
 text = u'''
 ''' # Copy verbatim into this box. Add a backslash (\) in front of (''') whereever necessary
-summary = "" # The edit summary
+summary = "" # The edit summary (or delete reason)
 wikilist = "listofwikis.txt" # List of wikis to edit.
 
 # Wikis to skip
